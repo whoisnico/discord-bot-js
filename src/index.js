511204f841c4289b243914bcd6cc6bf479b734ae
nicotrixxel <104150729+nicotrixxel@users.noreply.github.com>
2023-04-15 19:19:43 +0200

    Added OnMessage Event

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
+require("dotenv").config()
 const {Client, IntentsBitField} = require("discord.js")
 
 const client = new Client ({
     intents: [
-        IntentsBitField.Flags.Gu
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
     ]
 })
+
+client.on("ready", (c) => {
+    console.log("Bot is ready! Logged as "+c.user.tag);
+})
+
+client.on("messageCreate", (message) => {
+    if (message.author.bot) {
+        return
+    }
+
+    console.log(message.author.username + " >> " + message.content)
+    if (message.content == "hey"){
+        message.reply("Hey <@" + message.author + ">")
+    }
+})
+
+client.login(process.env.TOKEN)
